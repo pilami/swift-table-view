e1eb21f43b37c562331c1f03aaf17bb3f4d027c8
pilami <chait.pilami@gmail.com>
2015-05-05 23:32:38 +0530

    swift table view demo

---
+++ swifttableview/ViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ViewController.swift
+//  swifttableview
+//
+//  Created by Sai Chaitanya Manchikatla on 05/05/15.
+//  Copyright (c) 2015 Sai Chaitanya Manchikatla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController,UITableViewDataSource, UITableViewDelegate {
+    
+    let randomNames = ["bleh", "one more", "dude enough"]
+    let textCellIdentifier = "TextCell"
+    
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate =  self
+        tableView.dataSource = self
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    //Table view functions
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1;
+    }
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return randomNames.count
+    }
+    
+  
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(textCellIdentifier, forIndexPath: indexPath) as UITableViewCell
+        
+        let row = indexPath.row
+        cell.textLabel?.text = randomNames[row]
+        
+        return cell
+    }
+    
+   
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+        
+        let row = indexPath.row
+        println(randomNames[row])
+    }
+    
+
+}
+
---
+++ swifttableview/sampleviewcontroller1.swift
@@ -0,0 +1,26 @@
+//
+//  sampleviewcontroller1.swift
+//  swifttableview
+//
+//  Created by Sai Chaitanya Manchikatla on 05/05/15.
+//  Copyright (c) 2015 Sai Chaitanya Manchikatla. All rights reserved.
+//
+
+import UIKit
+
+class sampleviewcontroller1: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
